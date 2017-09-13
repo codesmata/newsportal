54a3383dfcca3031812c5610612c121ae5ea42bc
Busayo <arotimi.busayo@gmail.com>
2017-09-13 07:08:49 -0400

    Complted User Authentication Test

--- tests/Unit/Authentication/UserLoginTest.php
+++ tests/Unit/Authentication/UserLoginTest.php
@@ -4,17 +4,40 @@ namespace Tests\Unit\Authentication;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class UserLoginTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+
+    use DatabaseMigrations;
+
+    protected $user;
+
+    public function setup()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->user = factory(User::class, 'verifiedUser')->create();
+    }
+
+    public function testUserLoginFailure()
+    {
+        $credential = [
+            'email' => $this->user->email,
+            'password' => 'secretiswrong'
+        ];
+
+        $this->post('/login', $credential)
+            ->assertSessionHasErrors();
+    }
+
+    public function testUserLoginSuccess()
+    {
+        $credential = [
+            'email' => $this->user->email,
+            'password' => 'secret'
+        ];
+
+        $this->post('/login', $credential)
+            ->assertRedirect('/user-news');
     }
 }
